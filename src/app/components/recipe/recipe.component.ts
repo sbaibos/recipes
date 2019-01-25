34533be1bb4e649279e11c2b8a8e3deedc1e1a62
sotiris <sbaibos@gmail.com>
2019-01-25 15:24:32 +0200

    create and show simple data from component into app-component

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
-
+  
+  name = "sotos";
   constructor() { }
 
   ngOnInit() {
