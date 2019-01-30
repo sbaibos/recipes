816ceb5f5774ccc8a06260c2886956d31826946a
sotiris <sbaibos@gmail.com>
2019-01-30 11:22:08 +0200

    import http client in module , import and  initialize http in recipes.service.ts,function to get data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { RecipeComponent } from './components/recipe/recipe.component';
     RecipeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  
+  getRecipes(){
+	  
+	  return this.http.get('http://sbaibos.com/sotostheme/api/e-fresh-recipies.php');
+	  
+  }
   
   clicked(){
 	  
