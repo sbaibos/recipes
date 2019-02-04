4b127d75d59e5cde753e69905060e701b41b467e
sotiris <sbaibos@gmail.com>
2019-02-04 13:59:54 +0200

    bind simple drop down to ngmodel to show recipes selected

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -4,12 +4,14 @@ import {IngredientModel} from '../../models/ingredient.model';
 import {RECIPES} from '../../mockfiles/mock-recipes';
 import {INGREDIENTS} from '../../mockfiles/mock-ingredients';
 
+
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+  
   recipeModel: RecipeModel[];//initialize recipe model
   ingredientModel: IngredientModel[];//initialize ingredient model
   name = "recipe 1";
