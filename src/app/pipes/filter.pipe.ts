d052e6b28332125f8f3da4c9bb15acd5d460ff9c
sotiris <sbaibos@gmail.com>
2019-02-10 23:14:07 +0200

    re-write the pipe to properly work, polyffil.ts import for stackblitz

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -3,26 +3,30 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
- transform(value: any, args?: any): any {
-
-       // if(!value)return null;
-       // if(!args)return value;
-
-
-//------------older function-----------
-        
-//var result = value.filter(function(find){
- //return find.recipe_id ==args;
- 
-//});
-//------------older function ends-----------
-
-var result = value.filter(find => (find.recipe_id == args));
-
-       
-return result;
-      
-            //console.log(result);
-     
+  transform(items: any, filter: any, defaultFilter: boolean): any {
+    if (!filter){
+      return items;
     }
+
+    if (!Array.isArray(items)){
+      return items;
+    }
+
+    if (filter && Array.isArray(items)) {
+      let filterKeys = Object.keys(filter);
+
+      if (defaultFilter) {
+        return items.filter(item =>
+            filterKeys.reduce((x, keyName) =>
+                (x && new RegExp(filter[keyName], 'gi').test(item[keyName])) || filter[keyName] == "", true));
+      }
+      else {
+        return items.filter(item => {
+          return filterKeys.some((keyName) => {
+            return new RegExp(filter[keyName], 'gi').test(item[keyName]) || filter[keyName] == "";
+          });
+        });
+      }
+    }
+  }
 }
